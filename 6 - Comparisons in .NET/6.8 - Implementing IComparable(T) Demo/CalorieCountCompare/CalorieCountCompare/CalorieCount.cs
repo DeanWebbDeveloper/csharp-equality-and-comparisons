28be8856be03b73c6ad489b49ff557f7417858b7
deanwebbdeveloper <dean@deanwebbdeveloper.com>
2018-06-26 21:04:36 +0100

    Chapter 5, start Chapter 6
    Completed chapter 5, started chapter 6

---
+++ 6 - Comparisons in .NET/6.8 - Implementing IComparable(T) Demo/CalorieCountCompare/CalorieCountCompare/CalorieCount.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalorieCountCompare
+{
+    public struct CalorieCount : IComparable<CalorieCount>
+    {
+        #region IComparable<CalorieCount> Members
+
+        public int CompareTo(CalorieCount other)
+        {
+            return this._value.CompareTo(other._value);
+        }
+
+        #endregion
+
+        private float _value;
+
+        public float Value { get { return _value; } }
+
+        public CalorieCount(float value)
+        {
+            this._value = value;
+        }
+
+        public override string ToString()
+        {
+            return _value + " cal";
+        }
+
+    }
+}
---
+++ 6 - Comparisons in .NET/6.9 - Implementing the Comparison Operators/CalorieCountCompare/CalorieCountCompare/CalorieCount.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalorieCountCompare
+{
+    public struct CalorieCount : IComparable<CalorieCount>, IEquatable<CalorieCount>
+    {
+        #region IComparable<CalorieCount> Members
+
+        public int CompareTo(CalorieCount other)
+        {
+            return this._value.CompareTo(other._value);
+        }
+
+        public bool Equals(CalorieCount other)
+        {
+            return _value == other._value;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null)
+                return false;
+            if (!(obj is CalorieCount))
+                return false;
+            return _value == ((CalorieCount)obj)._value;
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+
+        public static bool operator ==(CalorieCount x, CalorieCount y)
+        {
+            return x._value == y._value;
+        }
+
+        public static bool operator !=(CalorieCount x, CalorieCount y)
+        {
+            return x._value != y._value;
+        }
+
+        public static bool operator <(CalorieCount x, CalorieCount y)
+        {
+            return x._value < y._value;
+        }
+
+        public static bool operator <=(CalorieCount x, CalorieCount y)
+        {
+            return x._value <= y._value;
+        }
+
+        public static bool operator >(CalorieCount x, CalorieCount y)
+        {
+            return x._value > y._value;
+        }
+
+        public static bool operator >=(CalorieCount x, CalorieCount y)
+        {
+            return x._value >= y._value;
+        }
+
+        #endregion
+
+        private float _value;
+
+        public float Value { get { return _value; } }
+
+        public CalorieCount(float value)
+        {
+            this._value = value;
+        }
+
+        public override string ToString()
+        {
+            return _value + " cal";
+        }
+
+    }
+}
